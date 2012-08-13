5b0587b818996a46a6cac85ef1b55ea16b147350
Austin Martinez <amartin857@gmail.com>
2012-08-13 11:10:36 -0700

    add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,25 @@ describe "StaticPages" do
   	it "should have the right title" do
 		  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
  	  end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorum ipsem")
+        FactoryGirl.create(:micropost, user: user, content: "zoopreme")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
+
   end
+
 
   describe "Help page" do
   	it "should have the content 'Help'" do
